b488e637c760c8259bc178cf17d2f68ea09ef783
inhere <in.798@qq.com>
2026-01-06 10:37:05 +0800

    feat(miglite): Implement the MigLite struct and its New and NewWithConfig constructors.
    - Add methods: Init、Up、Down、Skip、Status、Show methods

--- miglite.go
+++ miglite.go
@@ -1,17 +1,72 @@
 package miglite
 
-import "strings"
+import (
+	"github.com/gookit/miglite/pkg/command"
+	"github.com/gookit/miglite/pkg/config"
+)
 
-// Version represents the version of the application
-var Version = "0.1.0"
-var BuildTime = "2025-11-05T09:00:00Z"
-var GitCommit = "ab3cd4ef"
-var GoVersion = "1.21"
+// ConfigFn is a function type for updating the configuration
+type ConfigFn func(c *config.Config)
 
-// InitInfo initializes the version, build time, and git commit
-func InitInfo(version, goVer, buildTime, gitCommit string) {
-	Version = version
-	GoVersion = goVer
-	BuildTime = strings.Trim(buildTime, `'"`)
-	GitCommit = gitCommit
+// MigLite manage the migration
+type MigLite struct {
+	cfg *config.Config
+	// TODO add migrations by go code
+	//
+	// Example:
+	// 	mig.Add("2026...-add_user_table", `UP sql`, `DOWN sql`, options)
+	//
+	// migrations []*migration.Migration
+}
+
+// New creates a new MigLite instance, with optional configuration functions
+//
+//  - configFile: if not exist, will skip load it.
+func New(configFile string, fns ...ConfigFn) (*MigLite, error) {
+	cfg, err := config.Load(configFile)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, fn := range fns {
+		fn(cfg)
+	}
+	return NewWithConfig(cfg), nil
+}
+
+// NewWithConfig creates a new MigLite instance with a pre-configured Config
+func NewWithConfig(cfg *config.Config) *MigLite {
+	command.SetCfg(cfg)
+
+	return &MigLite{cfg: cfg}
+}
+
+// Init initializes the migration schema
+func (m *MigLite) Init(opt command.InitOption) error {
+	return command.HandleInit(opt)
+}
+
+// Up runs the migration up operation.
+func (m *MigLite) Up(opt command.UpOption) error {
+	return command.HandleUp(opt)
+}
+
+// Down runs the migration down operation.
+func (m *MigLite) Down(opt command.DownOption) error {
+	return command.HandleDown(opt)
+}
+
+// Skip skips some migration files.
+func (m *MigLite) Skip(opt command.SkipOption) error {
+	return command.HandleSkip(opt)
+}
+
+// Status shows the status of the migrations.
+func (m *MigLite) Status(opt command.StatusOption) error {
+	return command.HandleStatus(opt)
+}
+
+// Show displays all tables in the database.
+func (m *MigLite) Show(opt command.ShowOption) error {
+	return command.HandleShow(opt)
 }
---
+++ miglite_test.go
@@ -0,0 +1,51 @@
+package miglite_test
+
+import (
+	"github.com/gookit/goutil"
+	"github.com/gookit/miglite"
+	"github.com/gookit/miglite/pkg/command"
+	"github.com/gookit/miglite/pkg/config"
+)
+
+func ExampleNew() {
+	mig, err := miglite.New("miglite.yml", func(cfg *config.Config) {
+		// update config options
+	})
+	goutil.PanicIfErr(err) // handle error
+
+	// run up migrations
+	err = mig.Up(command.UpOption{
+		// ... options
+	})
+	goutil.PanicIfErr(err) // handle error
+
+	// other operations
+
+
+	// init migrations schema
+	err = mig.Init(command.InitOption{
+		// ... options
+	})
+	goutil.PanicIfErr(err) // handle error
+
+	// run down migrations
+	err = mig.Down(command.DownOption{
+		// ... options
+	})
+	goutil.PanicIfErr(err)
+
+	err = mig.Status(command.StatusOption{
+		// ... options
+	})
+	goutil.PanicIfErr(err) // handle error
+
+	err = mig.Show(command.ShowOption{
+		// ... options
+	})
+	goutil.PanicIfErr(err) // handle error
+
+	err = mig.Skip(command.SkipOption{
+		// ... options
+	})
+	goutil.PanicIfErr(err) // handle error
+}
