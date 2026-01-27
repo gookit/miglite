a7bff8f17c13ce648d0f9aa86954864335b3b407
inhere <in.798@qq.com>
2026-01-27 21:53:58 +0800

    refactor(miglite): Rename MigLite to Migrator and optimize database connection settings
    - Refactored the master struct name from MigLite to Migrator
    - Added the SetSqlDB method to set up database connections
    - Introduce the database package and implement SQL. DB connection settings function

--- miglite.go
+++ miglite.go
@@ -1,15 +1,18 @@
 package miglite
 
 import (
+	"database/sql"
+
 	"github.com/gookit/miglite/pkg/command"
 	"github.com/gookit/miglite/pkg/config"
+	"github.com/gookit/miglite/pkg/database"
 )
 
 // ConfigFn is a function type for updating the configuration
 type ConfigFn func(c *config.Config)
 
-// MigLite manage the migration
-type MigLite struct {
+// Migrator manage the migration
+type Migrator struct {
 	cfg *config.Config
 	// TODO add migrations by go code
 	//
@@ -19,10 +22,10 @@ type MigLite struct {
 	// migrations []*migration.Migration
 }
 
-// New creates a new MigLite instance, with optional configuration functions
+// New creates a new Migrator instance, with optional configuration functions
 //
 //  - configFile: if not exist, will skip load it.
-func New(configFile string, fns ...ConfigFn) (*MigLite, error) {
+func New(configFile string, fns ...ConfigFn) (*Migrator, error) {
 	cfg, err := config.Load(configFile)
 	if err != nil {
 		return nil, err
@@ -34,39 +37,44 @@ func New(configFile string, fns ...ConfigFn) (*MigLite, error) {
 	return NewWithConfig(cfg), nil
 }
 
-// NewWithConfig creates a new MigLite instance with a pre-configured Config
-func NewWithConfig(cfg *config.Config) *MigLite {
+// NewWithConfig creates a new Migrator instance with a pre-configured Config
+func NewWithConfig(cfg *config.Config) *Migrator {
 	command.SetCfg(cfg)
+	return &Migrator{cfg: cfg}
+}
 
-	return &MigLite{cfg: cfg}
+// SetSqlDB sets the database connection
+func (m *Migrator) SetSqlDB(db *sql.DB) {
+	dbCfg := m.cfg.Database
+	command.SetDB(database.NewWithSqlDB(dbCfg.Driver, db))
 }
 
 // Init initializes the migration schema
-func (m *MigLite) Init(opt command.InitOption) error {
+func (m *Migrator) Init(opt command.InitOption) error {
 	return command.HandleInit(opt)
 }
 
 // Up runs the migration up operation.
-func (m *MigLite) Up(opt command.UpOption) error {
+func (m *Migrator) Up(opt command.UpOption) error {
 	return command.HandleUp(opt)
 }
 
 // Down runs the migration down operation.
-func (m *MigLite) Down(opt command.DownOption) error {
+func (m *Migrator) Down(opt command.DownOption) error {
 	return command.HandleDown(opt)
 }
 
 // Skip skips some migration files.
-func (m *MigLite) Skip(opt command.SkipOption) error {
+func (m *Migrator) Skip(opt command.SkipOption) error {
 	return command.HandleSkip(opt)
 }
 
 // Status shows the status of the migrations.
-func (m *MigLite) Status(opt command.StatusOption) error {
+func (m *Migrator) Status(opt command.StatusOption) error {
 	return command.HandleStatus(opt)
 }
 
 // Show displays all tables in the database.
-func (m *MigLite) Show(opt command.ShowOption) error {
+func (m *Migrator) Show(opt command.ShowOption) error {
 	return command.HandleShow(opt)
 }
